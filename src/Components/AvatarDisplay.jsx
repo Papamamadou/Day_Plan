e3dd9bb821673db32fd3013eb4d25f6f31424148
Papamamadou <diallopapamamadou5@gmail.com>
2023-12-17 16:39:14 +0100

    Istallation dependencies Axios Express Cors Dotenv et Nodemon initailisation of ticket page

--- src/Components/AvatarDisplay.jsx
+++ src/Components/AvatarDisplay.jsx
@@ -1,10 +1,16 @@
 import blankAvatar from '../images/blankAvatar.png'
-export default function AvatarDisplay({ticket}) {
-    return (
-      <div className="avatar-container">
-        <div className="img-container">
-          <img src={ticket.avatar? ticket.avatar : blankAvatar} alt={'Photo of' + ticket.owner }/>
-        </div>
-      </div>
-    )
-  }
+
+const AvatarDisplay = ({ ticket }) => {
+  return (
+<div className="avatar-container">
+<div className="img-container">
+<img src={ticket.avatar ? ticket.avatar : blankAvatar} alt={'photo of ' + ticket.owner} />
+</div>
+</div>
+  )
+}
+
+export default AvatarDisplay
+
+
+
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,18 +1,21 @@
 import { useNavigate } from "react-router-dom"
 import logo from '../images/star.png'
-export default function Nav() {
+
+const Nav = () => {
 
   const navigate = useNavigate()
-    return (
-      <nav>
-        <div className="logo-container">
-          <img src={logo} alt="logo "/>
-        </div>
 
-        <div className="controls-container">
-          <div className="icon" onClick={() =>navigate('/ticket')}>+</div>
-          <div className="icon" onClick={() =>navigate('/')}>«</div>
-        </div>
-      </nav>
-    )
-  }
+  return (
+    <nav>
+      <div className="logo-container" onClick={() => navigate('/')}>
+        <img src={logo} alt="logo"/>
+      </div>
+      <div className="controls-container">
+        <div className="icon" onClick={() => navigate('/ticket')}>➕</div>
+        <div className="icon" onClick={() => navigate('/')}>❮❮</div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
