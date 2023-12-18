48108f4f091dd32f121957abff54c7c6d2c01f29
Papamamadou <diallopapamamadou5@gmail.com>
2023-12-18 23:42:03 +0100

    Ticket modifying

--- src/Pages/TicketPage.jsx
+++ src/Pages/TicketPage.jsx
@@ -18,6 +18,8 @@ const TicketPage = ({ editMode }) => {
       [name]: value,
     }))
   }
+const categories =['test 1', 'test 2']
+
   console.log(formData)
 
   const handleSubmit = async (e) => {
@@ -32,9 +34,6 @@ const TicketPage = ({ editMode }) => {
     }
   }, [])
 
-  console.log('EDITcategories', categories)
-  console.log('formData', formData.status)
-  console.log('formData.status', formData.status === 'stuck')
   return (
     <div className="ticket">
       <h1>{editMode ? 'Update Your Ticket' : 'Create a Ticket'}</h1>
@@ -51,7 +50,92 @@ const TicketPage = ({ editMode }) => {
               value={formData.title}
             />
 
-           
+            <label htmlFor="description">Description</label>
+            <input
+              id="description"
+              name="description"
+              type="text"
+              onChange={handleChange}
+              required={true}
+              value={formData.description}
+            />
+
+            <label>Category</label>
+            <select
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+            >
+              {categories?.map((category, _index) => (
+                <option key={_index}value={category}>{category}</option>
+              ))}
+            </select>
+
+            <label htmlFor="new-category">New Category</label>
+            <input
+              id="new-category"
+              name="category"
+              type="text"
+              onChange={handleChange}
+              value={formData.category}
+            />
+
+            <label htmlFor="priority-1">1</label>
+              <input
+                id="priority-1"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={2}
+                checked={formData.priority == 2}
+              />
+              <label htmlFor="priority-2">2</label>
+              <input
+                id="priority-2"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={3}
+                checked={formData.priority == 3}
+              />
+              <label htmlFor="priority-3">3</label>
+              <input
+                id="priority-3"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={4}
+                checked={formData.priority == 4}
+              />
+              <label htmlFor="priority-4">4</label>
+              <input
+                id="priority-4"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={5}
+                checked={formData.priority == 5}
+              />
+              <label htmlFor="priority-5">5</label>
+              <input
+                id="priority-5"
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                value={5}
+                checked={formData.priority == 5}
+              />
+              <input
+                  type="range"
+                  id="progress"
+                  name="progress"
+                  value={formData.progress}
+                  min="0"
+                  max="100"
+                  onChange={handleChange}
+                />
+              <label htmlFor="progress">Progress</label>
+            
           </section>
         </form>
       </div>
